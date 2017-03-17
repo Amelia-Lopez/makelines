a1b5a8b3d8ed021fb76770b7b428f0dc793458df
Mario Lopez <mariolj@gmail.com>
2017-03-16 22:36:07 -0500

    Updated Ticker unit test to use a Spock Mock instead of an inner class

--- src/test/groovy/com/mariolopezjr/tetris/controller/TickerSpec.groovy
+++ src/test/groovy/com/mariolopezjr/tetris/controller/TickerSpec.groovy
@@ -2,27 +2,17 @@ package com.mariolopezjr.tetris.controller
 
 import spock.lang.Specification
 
-import java.util.concurrent.atomic.AtomicInteger
-
 /**
  * Created by mlopez on 3/16/17.
  */
 class TickerSpec extends Specification {
 
-    static class TickerListenerImpl implements TickListener {
-        final def count = new AtomicInteger()
-
-        @Override
-        void tick() {
-            count.incrementAndGet()
-        }
-    }
+    def tickerListener = Mock(TickListener)
 
     def "should call tick() immediately after starting ticker"() {
         given: "the ticker interval is 10 seconds"
         def intervalInMillis = 10_000
         def ticker = new Ticker()
-        def tickerListener = new TickerListenerImpl()
         ticker.setInterval(intervalInMillis).setTickListener(tickerListener)
 
         when: "the ticker is started"
@@ -32,7 +22,7 @@ class TickerSpec extends Specification {
         sleep(1_000)
 
         then: "the ticker listener's tick() was called once"
-        tickerListener.count.get() == 1
+        1 * tickerListener.tick()
 
         cleanup:
         ticker.stop()
@@ -42,7 +32,6 @@ class TickerSpec extends Specification {
         given: "the ticker interval is 1 second"
         def intervalInMillis = 1_000
         def ticker = new Ticker()
-        def tickerListener = new TickerListenerImpl()
         ticker.setInterval(intervalInMillis).setTickListener(tickerListener)
 
         when: "the ticker is started"
@@ -52,7 +41,7 @@ class TickerSpec extends Specification {
         sleep(1_500)
 
         then: "the ticker listener's tick() was called twice, once immediately and once more after the interval"
-        tickerListener.count.get() == 2
+        2 * tickerListener.tick()
 
         cleanup:
         ticker.stop()
@@ -62,7 +51,6 @@ class TickerSpec extends Specification {
         given: "the ticker interval is 1 second"
         def intervalInMillis = 1_000
         def ticker = new Ticker()
-        def tickerListener = new TickerListenerImpl()
         ticker.setInterval(intervalInMillis).setTickListener(tickerListener)
 
         when: "the ticker is started"
@@ -72,7 +60,7 @@ class TickerSpec extends Specification {
         sleep(2_500)
 
         then: "the ticker listener's tick() was called three times in total, once immediately and then twice more"
-        tickerListener.count.get() == 3
+        3 * tickerListener.tick()
 
         cleanup:
         ticker.stop()
@@ -82,21 +70,19 @@ class TickerSpec extends Specification {
         given: "the ticker interval is 1 second"
         def intervalInMillis = 1_000
         def ticker = new Ticker()
-        def tickerListener = new TickerListenerImpl()
         ticker.setInterval(intervalInMillis).setTickListener(tickerListener)
 
         when: "test waits for 1.5 seconds"
         sleep(1_500)
 
         then: "the ticker listener's tick() is never called"
-        tickerListener.count.get() == 0
+        0 * tickerListener.tick()
     }
 
     def "should stop calling tick() after ticker is stopped"() {
         given: "the ticker interval is 1 second"
         def intervalInMillis = 1_000
         def ticker = new Ticker()
-        def tickerListener = new TickerListenerImpl()
         ticker.setInterval(intervalInMillis).setTickListener(tickerListener)
 
         when: "the ticker is started"
@@ -106,18 +92,15 @@ class TickerSpec extends Specification {
         sleep(1_500)
 
         then: "the ticker listener's tick() was called twice"
-        tickerListener.count.get() == 2
+        2 * tickerListener.tick()
 
         when: "the ticker is stopped"
         ticker.stop()
-
-        and: "the ticker listener's count is reset"
-        tickerListener.count.set(0)
 
         and: "test waits for 2 seconds"
         sleep(2_000)
 
         then: "the ticker listener's tick() was not called again"
-        tickerListener.count.get() == 0
+        0 * tickerListener.tick()
     }
 }
